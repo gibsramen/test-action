f9e72c5c7f9626532287a17530e965b623a143be
Gibraan Rahman <gibraan.rahman@czbiohub.org>
2024-06-27 15:09:54 -0700

    Try more stuff

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const memory = core.getInput('memory');
-  console.log(`Memory: ${memory}!`);
-  const slurm_command = "sbatch my_script.sh --mem=".concat(memory);
-  core.setOutput("slurm_command", slurm_command);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const bruno_user = core.getInput('bruno_user');
+    const url = core.getInput('url');
+    const repository = core.getInput('repository');
+    const org_regex = /czbiohub-sf\//
+
+    const command = (
+        'ssh -i ~/.ssh/gh_action '
+            + bruno_user + '@' + url
+            + ' submit_runner '
+            + repository.replace(org_regex, '')
+    );
+    core.setOutput("command", command);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`Repo name: ${payload.repository.name}`);
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
